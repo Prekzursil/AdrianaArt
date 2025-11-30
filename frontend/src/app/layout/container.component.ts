b7091767dd696b96c6b1c38876b00a75f9204144
Prekzursil <a_visalon@yahoo.com>
2025-11-30 20:47:01 +0200

    fix: import NgClass in container component

--- frontend/src/app/layout/container.component.ts
+++ frontend/src/app/layout/container.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-container',
   standalone: true,
+  imports: [NgClass],
   template: `
     <div class="max-w-6xl mx-auto px-4 sm:px-6 w-full" [ngClass]="classes">
       <ng-content></ng-content>
