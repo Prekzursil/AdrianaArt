f0b3240b9aa5c787419831a74312281d077857c4
Prekzursil <a_visalon@yahoo.com>
2025-12-01 05:57:43 +0200

    test: add http testing module for app component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, AppComponent]
+      imports: [RouterTestingModule, HttpClientTestingModule, AppComponent]
     }).compileComponents();
   });
 
